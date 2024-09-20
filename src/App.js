5770011a31ac03ba5b1292794af21d2b47f108f9
Aletswalo <aletswalo19@gmail.com>
2024-09-20 21:45:49 +0200

    Added the Git repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <h1>
       Weather App
+      </h1>
+      <footer>
+        This project was coded by{""} <a href="https://github.com/Aletswalo" target="_blank"> Annah Letswalo </a>{""} and is {""}
+        <a href="https://github.com/Aletswalo/weather-app-react" target="_blank">
+        open-sourced on GitHub
+        </a>
+      </footer>
       
     </div>
   );
