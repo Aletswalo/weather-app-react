300ad38df69f5caee630a43809836845ae103cc1
Aletswalo <aletswalo19@gmail.com>
2024-09-22 23:37:40 +0200

    Added a hosting platform link

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
         This project was coded by{""} <a href="https://github.com/Aletswalo" target="_blank" className="text-decoration-none" rel="noreferrer"> Annah Letswalo </a>{""} and is {""}
         <a href="https://github.com/Aletswalo/weather-app-react" target="_blank" className="text-decoration-none" rel="noreferrer">
         open-sourced on GitHub
-        </a>
+        </a> and <a href="https://teal-parfait-2800f2.netlify.app/" className="text-decoration-none">hosted on Netlify</a>
       </footer>
       </div>
     </div>
